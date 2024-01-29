bc10361bf93e29a9baf5e7e4deac0bd5dbed4e6f
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-01-30 01:25:29 +0500

    feat: Add logic for the TeacherActivity

--- app/src/main/java/org/hse/base/TeacherActivity.java
+++ app/src/main/java/org/hse/base/TeacherActivity.java
@@ -3,12 +3,88 @@ package org.hse.base;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class TeacherActivity extends AppCompatActivity {
+
+    private TextView time, status, subject, cabinet, corp, teacher;
+    private Date currentTime;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
+
+        final Spinner spinner = findViewById(R.id.groupList);
+
+        List<Group> groups = new ArrayList<>();
+        initGroupList(groups);
+
+        ArrayAdapter<?> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, groups);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Object item = adapter.getItem(position);
+                Log.d("select", "onItemSelected: " + item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        time = findViewById(R.id.time);
+        initTime();
+
+        status = findViewById(R.id.status);
+        subject = findViewById(R.id.subject);
+        cabinet = findViewById(R.id.cabinet);
+        corp = findViewById(R.id.corp);
+        teacher = findViewById(R.id.teacher);
+        initData();
+    }
+
+    private void initGroupList(List<Group> groups){
+        groups.add(new Group(1, "Преподаватель 1"));
+        groups.add(new Group(2, "Преподаватель 2"));
+    }
+
+    private void initTime() {
+        currentTime = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm, EEEE",
+                Locale.forLanguageTag("RU"));
+        String[] dateSplit = simpleDateFormat.format(currentTime).split(" ");
+        String timeText = getString(R.string.label_time) + dateSplit[0] + " " +
+                dateSplit[1].substring(0,1).toUpperCase() +
+                dateSplit[1].substring(1);
+        time.setText(timeText);
+    }
+
+    private void initData() {
+        status.setText(R.string.label_status_base);
+        subject.setText(R.string.label_subject_base);
+        cabinet.setText(R.string.label_cabinet_base);
+        corp.setText(R.string.label_corp_base);
+        teacher.setText(R.string.label_choose_teacher);
     }
 }
